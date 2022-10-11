fd9c5e31171fb13c4f287bde0fa54c00f02aeb91
William Arvidsson <william.arvidsson@gmail.com>
2022-10-11 21:07:01 +0200

    Cooly

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Very Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Very Freezing", "Bracing", "Chilly", "Cooly", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
